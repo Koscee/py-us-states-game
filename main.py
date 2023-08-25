e5ec874913b41aa78e9c9d463aafe86eb5219691
Koscee <52253099+Koscee@users.noreply.github.com>
2023-08-26 01:09:31 +0800

    feat: implement game core functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,6 +7,28 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="Whats another state's name?")
+total_score = 50
+score = 0
+correct_guesses = []
+location_data = pandas.read_csv("50_states.csv")
+
+while score < total_score:
+    input_title = 'Guess the State' if score == 0 else f'{score}/{total_score} States Correct'
+    answer_state = screen.textinput(title=input_title, prompt="Whats another state's name?")
+    answer_state = answer_state.title() if answer_state else ''
+    found_data = location_data[location_data.state == answer_state]
+
+    is_marked = answer_state in correct_guesses
+
+    if not is_marked and not found_data.empty:
+        state = found_data.state.item()
+        position = (int(found_data.x), int(found_data.y))
+        marker = turtle.Turtle()
+        marker.hideturtle()
+        marker.penup()
+        marker.goto(position)
+        marker.write(state, align="center", font=("Arial", 9, "bold"))
+        correct_guesses.append(state)
+        score += 1
 
 screen.exitonclick()

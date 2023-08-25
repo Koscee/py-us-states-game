40db3f8021a4d9b13d0b54afe2e0a190bb97a3ca
Koscee <52253099+Koscee@users.noreply.github.com>
2023-08-25 23:09:48 +0800

    feat: add map and text input prompt

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
-
+answer_state = screen.textinput(title="Guess the State", prompt="Whats another state's name?")
 
 screen.exitonclick()

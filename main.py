b30e303314bd8df00c8099b77f63d16713784174
Koscee <52253099+Koscee@users.noreply.github.com>
2023-08-26 23:45:20 +0800

    feat: add ability to exit game and store missing states to a file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
+screen.setup(width=725, height=491)
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
@@ -16,8 +17,14 @@ while score < total_score:
     input_title = 'Guess the State' if score == 0 else f'{score}/{total_score} States Correct'
     answer_state = screen.textinput(title=input_title, prompt="Whats another state's name?")
     answer_state = answer_state.title() if answer_state else ''
-    found_data = location_data[location_data.state == answer_state]
 
+    if answer_state == 'Exit':
+        all_states = location_data.state.to_list()
+        missing_states = [s for s in all_states if s not in correct_guesses]
+        pandas.DataFrame({"States to Learn": missing_states}).to_csv("states_to_learn.csv")
+        break
+
+    found_data = location_data[location_data.state == answer_state]
     is_marked = answer_state in correct_guesses
 
     if not is_marked and not found_data.empty:
@@ -30,5 +37,3 @@ while score < total_score:
         marker.write(state, align="center", font=("Arial", 9, "bold"))
         correct_guesses.append(state)
         score += 1
-
-screen.exitonclick()
